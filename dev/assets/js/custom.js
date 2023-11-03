1b8777e8aebf2ae82446ecb2acbe16f4a5cbdc85
Andrey5587 <andreyzp1977@gmail.com>
2023-11-03 13:51:24 +0200

    feat: first-screen mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,34 @@
-  
+$(document).ready(function() {
+
+    $('.first-screen-slider').slick({
+        dots:true,
+        appendArrows:'.first-screen-nav',
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                arrows: false,
+              }
+            },
+            
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+}
+)
+
+
+const burger = document.querySelector('.hamburger');
+
+burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active')
+})
+
+
+
+
+document.getElementById('fileinput').onchange = function(){
+    document.getElementById('file-Name').innerHTML = this.value;
+}
